1f45199858b5f16cdc39d7bd9e82828f53b68fc6
Aadit Thanekar <162476472+AadCode7@users.noreply.github.com>
2026-08-11 17:04:23 +0530

    Refactor model_loader.py for improved structure

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -3,12 +3,12 @@ import sys
 import json
 import asyncio
 from dotenv import load_dotenv
-from research_analyst.utils.config_loader import load_config
+from utils.config_loader import load_config
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
 from langchain_groq import ChatGroq
-from research_analyst.logger import GLOBAL_LOGGER as log
-from research_analyst.exception.custom_exception import ResearchAnalystException
+from logger import GLOBAL_LOGGER as log
+from exception.custom_exception import ResearchAnalystException
 
 
 class ApiKeyManager:
